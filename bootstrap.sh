6fe7273438094c227783dc0836014cb1178147ae
Juho Friman <juho.friman@uta.fi>
2014-10-19 14:53:45 +0300

    It creates vagrant ingres user with password vagrant.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Hello Hello, we do ingres community edition installation for you"
+echo "Hello Hello, we do ingres community edition installation for you..."
 
 cd /vagrant/place-ingres-here
 
-[ ! -r ing*.tgz ] && echo "No ingres*.tgz found from place-ingres-here. Acquire it and place it" && exit 1
-
-[[ -d ingres* ]] && echo "Seems like shared path has unpacked ingres, removing..." && rm -rf ingres*
+[ ! -r ing*.tgz ] && echo "No ingres*.tgz found from /vagrant/place-ingres-here. Acquire it." && exit 1
 
 INGRES_PACKAGE=`find / -type f -name "ing*.tgz" -print`
 echo "Unpacking $INGRES_PACKAGE"
@@ -15,9 +13,10 @@ tar -xf $INGRES_PACKAGE
 
 groupadd ww_eng
 
-echo "Installing ingres express..."
+echo "Installing ingres express and wait until it is running..."
 cd ingres*
 echo "Y" | sh ingres_express_install.sh
+wait
 
 if [ $? == 6 ]
   then
@@ -29,3 +28,11 @@ cp /home/ingres/.ingIIbash /home/vagrant/.ingIIbash
 chown vagrant:vagrant /home/vagrant/.ingIIbash
 echo "source .ingIIbash" >> /home/vagrant/.bash_profile
 echo "source .ingIIbash" >> /home/ingres/.bash_profile
+
+mkdir /home/ingres/spec
+cp /vagrant/spec/vagrant-user-spec.sql /home/ingres/spec/vagrant-user-spec.sql
+
+# This should be done to every sql-file in spec
+su - ingres -c "sql iidbdb < /home/ingres/spec/vagrant-user-spec.sql"
+
+echo "Yeow!! vagrant ssh and start ingresing! See password from spec/vagrant-user-spec.sql"
